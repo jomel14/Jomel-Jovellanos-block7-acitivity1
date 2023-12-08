1d8f8c6a19619d36bfbdbfd6d43bb6d63253e647
jomel jovellanos <jomeljovellanos@gmail.com>
2023-12-08 10:59:19 +0800

    quiz1

---
+++ app/src/main/java/com/example/jomel_jovellanos_block7_quiz1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jomel_jovellanos_block7_quiz1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
